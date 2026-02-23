9692f1fe73a520fcf5370a8b9d5f0f27c81abc98
Edi Sadikaj <sadikaj.edi@pm.me>
2026-02-23 11:18:37 +0100

    Implement Runnable interface for job execution and update project settings

--- src/main/java/_02_FirstThread/_02_01_Process.java
+++ src/main/java/_02_FirstThread/_02_01_Process.java
@@ -10,5 +10,17 @@ public class _02_01_Process {
         //2. a job for the thread to do
 
 
+        //the job:
+        Runnable runnable1 = new _02_01_Job(" Job1");
+        Runnable runnable2 = new _02_01_Job(" Job2");
+
+        //THREAD:
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        //LETS GO AND START!
+        thread1.start(); //non-blocking
+        thread2.start();
+
     }
 }
